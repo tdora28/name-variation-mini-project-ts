13701e9969f25e21a679f324df82051f078691cf
tdora28 <tokaidora88@gmail.com>
2024-02-22 15:22:59 +0200

    Implement getNumberOfChars

--- src/variations.ts
+++ src/variations.ts
@@ -1,24 +1,20 @@
-export function getNumberOfChars(name) {
+export function getNumberOfChars(name: string): number {
   // number of characters in: name
-
+  return name.length;
 }
 
-export function getFirstChar(name) {
+export function getFirstChar(name: string) {
   // first character of: name
-
 }
 
-export function getLastChar(name) {
+export function getLastChar(name: string) {
   // last character of: name
-
 }
 
-export function getLower(name) {
+export function getLower(name: string) {
   // name all in lower case (example: "ABC" becomes "abc")
-
 }
 
-export function getUpper(name) {
+export function getUpper(name: string) {
   // name all in upper case (example: "abc" becomes "ABC")
-
 }
