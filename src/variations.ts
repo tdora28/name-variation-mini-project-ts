da0dc74c26701921c3d89e61913ea44517202b37
tdora28 <tokaidora88@gmail.com>
2024-02-22 15:20:51 +0200

    Add initial files

---
+++ src/variations.ts
@@ -0,0 +1,24 @@
+export function getNumberOfChars(name) {
+  // number of characters in: name
+
+}
+
+export function getFirstChar(name) {
+  // first character of: name
+
+}
+
+export function getLastChar(name) {
+  // last character of: name
+
+}
+
+export function getLower(name) {
+  // name all in lower case (example: "ABC" becomes "abc")
+
+}
+
+export function getUpper(name) {
+  // name all in upper case (example: "abc" becomes "ABC")
+
+}
